0c4a04cf7998a9dd34b39d95ca28fea513a81a13
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-01-10 00:01:59 +0530

    complete CURD functionality

--- tweetApp/tweet/views.py
+++ tweetApp/tweet/views.py
@@ -33,7 +33,22 @@ def tweet_create(request):
 def tweet_edit(request ,tweet_id):
     tweet =get_object_or_404(Tweet ,pk =tweet_id ,user =request.user)
     if request.method =='POST':
+        form =TweetForm(request.POST, request.FILES,instance=tweet)
+        if form.is_valid():
+            tweet =form.save(commit=False)
+            tweet.save =request.user
+            tweet.save()
+            return redirect('tweet_list')
        
     else:
         form =TweetForm(instance=tweet) # form having somthing prefill
     return render(request,'tweet_form.html',{'form':form})
+
+
+def tweet_delete(request,tweet_id):
+    tweet =get_object_or_404(Tweet,pk=tweet_id,user =request.user)
+    if request.method =='POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request,'tweet_confirm_delete.html',{'tweet':tweet})
+    
