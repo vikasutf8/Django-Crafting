3ea89b7c84fc6966a2201c6b60fd2b653220d757
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-01-09 23:36:13 +0530

    using inbuild forms

---
+++ tweetApp/tweet/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Tweet
+
+#using inbuild forms
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model =Tweet
+        fields =['text','photo']
